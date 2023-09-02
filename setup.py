3fcf98934cd47d7dcc820edfc929e0b9fc55e171
Paras Kapoor <kapoorparas0001@gmail.com>
2023-09-02 11:57:34 +0530

    Setup file updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name ="src",
-    version="0.0.1",
+    version="0.0.2",
     author="Paras Kapoor",
     description="ANN implementation on mnist dataset",
     long_description=long_description,
